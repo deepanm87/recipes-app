feba227eff85c5fcfb28c791c00428e08175e65a
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-07-02 13:28:41 -0400

    added search bar for pantries page

--- app/routes/app/pantry.tsx
+++ app/routes/app/pantry.tsx
@@ -1,18 +1,41 @@
 import { PantryShelf } from "@prisma/client"
-import { useLoaderData } from "react-router"
+import { useLoaderData, type LoaderArgs, Form } from "react-router"
 import { getAllShelves } from "~/models/pantry-shelf.server"
 import classNames from "classnames"
+import { SearchIcon } from "../../components/icons"
 
-export async function loader() {
-    const shelves = await getAllShelves()
+export async function loader({ request }: LoaderArgs) {
+    const url = new URL(request.url)
+    const q = url.searchParams.get("q")
+    const shelves = await getAllShelves(q)
     return { shelves }
 }
 
 export default function Pantry() {
-    const data = useLoaderData<typeof loader>()
+    const data = useLoaderData() as LoaderData
+    const [searchParams] = useSearchParams()
+    const navigation = useNavigation()
+
+    const isSearching = navigation.formData?.has("q")
     return (
         <div>
-            <ul className={classNames("flex gap-8 overflow-x-auto", "snap-x snap-mandatory md:snap-none")}>
+            <Form className={classNames(
+                "flex border-2 border-gray-300 rounded-md", 
+                "focus-within:border-primary md:w-80",
+                { "animate-pulse": isSearching}
+                )}>
+                <button className="px-2 mr-1"><SearchIcon /></button>
+                <input 
+                    defaultValue={searchParams.get("q") ?? ""}
+                    type="text"
+                    name="q"
+                    autoComplete="off"
+                    placeholder="Search shelves..."
+                    className="w-full py-3 px-2 outline-none"
+                />
+                
+            </Form>
+            <ul className={classNames("flex gap-8 overflow-x-auto mt-4", "snap-x snap-mandatory md:snap-none")}>
                 { data.shelves.map( shelf => 
                     <li 
                         key={shelf.id} 
